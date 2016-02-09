d317c10dc3c700226d9f99fcb3980fa0b6bcc830
jakedemian <jakedemian@gmail.com>
2016-02-08 23:19:11 -0500

    added menu bar and refactored some of the styling

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var BrowserWindow = require('browser-window');
 
 app.on('ready', function() {
 	var mainWindow = new BrowserWindow({
-		width: 1200,
-		height: 800,
+		width: 800,
+		height: 600,
 		frame: false
 	});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
